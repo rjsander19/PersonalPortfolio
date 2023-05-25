5a90efe91ced2859c00b18631b059368cc020867
rjsander <rjsander19@gmail.com>
2023-05-25 13:58:44 -0700

    update to html landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,7 +5,7 @@ const Landing = (props) => {
     return(
 <>
 <h1>Welcome to Roxanne's Software Engineering Portfolio</h1>
-<p>Thank you so much for visiting my page. Please feel free to visit the links to my LinkedIn and Github profiles, which are displayed at the bottom of the page. Otherwise, any inquiries can be sent to me directly via email at rjsander19@gmail.com</p>
+<p>Thank you so much for visiting my portfolio website. Please feel free to visit the links to my LinkedIn and Github profiles, which are displayed at the bottom of the page. Otherwise, any inquiries can be sent to me directly via email at rjsander19@gmail.com</p>
 <h2>Please click on the logo at the top left of your screen to proceed to the project display page</h2>
 
 
