fcc0f1009925a997f41c8218124d5d0c649544a1
rjsander <rjsander19@gmail.com>
2023-05-30 14:40:47 -0700

    add linkedin logo to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,12 @@ const Footer = (props) => {
         <footer className="footer">
             <Link to="https://www.linkedin.com/in/roxanne-sanderson-487873a6/">
                 <div className="Linkedin">
-                    <p><strong>Linkedin: </strong>https://www.linkedin.com/in/roxanne-sanderson-487873a6/ </p>
+                <p><img src="https://i.ibb.co/7zQ1wR6/Screen-Shot-2023-05-30-at-2-39-43-PM.png" alt="linkedinlogo" className="linkedinlogo"/>https://www.linkedin.com/in/roxanne-sanderson-487873a6/ </p>
                 </div>
             </Link>
             <Link to="https://github.com/rjsander19">
                 <div className="Github">
-                    <p><img src="https://i.ibb.co/2tpdBR4/Screen-Shot-2023-05-30-at-2-31-59-PM.png" alt="githublogo" className="githublogo"/>https://github.com/rjsander19</p>
+                    <p><img src="https://i.ibb.co/2tpdBR4/Screen-Shot-2023-05-30-at-2-31-59-PM.png" alt="githublogo" className="githublogo"/> https://github.com/rjsander19</p>
                 </div>
             </Link>
                 <div className="Email">
