c327e2b7b69c3b090dae55fa2677bc72215dc295
rjsander <rjsander19@gmail.com>
2023-06-05 16:51:29 -0700

    add onclick to all in resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,79 +5,79 @@ import {Link} from "react-router-dom";
 const Resume = (props) => {
   const [activeStates, setActiveStates] = useState(Array(6).fill(false));
 
-  const handleJobClick = () => {
+  const handleJobClick = (index) => {
     const newActiveStates = [...activeStates];
     newActiveStates[index] = !newActiveStates[index];
     setActiveStates(newActiveStates);
   };
 
-  return(
-<>
-<div className="show-container">
-    <div className="card-row">
-        <div className="toprow">
-        <div className={`job1 ${activeStates[0] ? 'active' : ''}`} onClick={() => handleJobClick(0)}>
-            <div className="job1text">
+  return (
+    <>
+      <div className="show-container">
+        <div className="card-row">
+          <div className="toprow">
+            <div className={`job1 ${activeStates[0] ? 'active' : ''}`} onClick={() => handleJobClick(0)}>
+              <div className="job1text">
                 <p><strong>General Assembly</strong></p>
-                    <p>March 2023 - May 2023</p>
-                      <p>
-                      Completed the Software Engineering Immersive Bootcamp through General Assembly
-                      </p>
-                </div>
+                <p>March 2023 - May 2023</p>
+                <p>
+                  Completed the Software Engineering Immersive Bootcamp through General Assembly
+                </p>
+              </div>
             </div>
-        </div>
-        <div className="secondrow">
-        <div className={`job2 ${activeStates[1] ? 'active' : ''}`} onClick={() => handleJobClick(1)}>
+          </div>
+          <div className="secondrow">
+            <div className={`job2 ${activeStates[1] ? 'active' : ''}`} onClick={() => handleJobClick(1)}>
               <div className="job2text">
                 <p><strong>Associated Property Management</strong></p>
-                  <p>March 2022 - November 2022</p>
-                    <p>Community Association Manager</p>
-                      <p>As a community manager I was responsible for building and maintaining a business relationship with a portfolio of clients</p>
-                </div>
+                <p>March 2022 - November 2022</p>
+                <p>Community Association Manager</p>
+                <p>As a community manager, I was responsible for building and maintaining a business relationship with a portfolio of clients</p>
+              </div>
             </div>
-        </div>
-        <div className="thirdrow">
-        <div className={`job3 ${activeStates[2] ? 'active' : ''}`} onClick={() => handleJobClick(2)}>
+          </div>
+          <div className="thirdrow">
+            <div className={`job3 ${activeStates[2] ? 'active' : ''}`} onClick={() => handleJobClick(2)}>
               <div className="job3text">
-                  <p><strong>Associa</strong></p>
-                    <p>November 2020 - February 2022</p>
-                      <p>Community Association Manager</p>
-                        <p>Some responsibilities I had in this role were: addressing maintenance and financial concerns that are reported by homeowners and board members</p>
-                </div>
+                <p><strong>Associa</strong></p>
+                <p>November 2020 - February 2022</p>
+                <p>Community Association Manager</p>
+                <p>Some responsibilities I had in this role were: addressing maintenance and financial concerns that are reported by homeowners and board members</p>
+              </div>
             </div>
-        </div>
-        <div className="fourthrow">
-        <div className={`job4 ${activeStates[3] ? 'active' : ''}`} onClick={() => handleJobClick(3)}>
+          </div>
+          <div className="fourthrow">
+            <div className={`job4 ${activeStates[3] ? 'active' : ''}`} onClick={() => handleJobClick(3)}>
               <div className="job4text">
                 <p><strong>Associa</strong></p>
-                  <p>August 2019 - November 2020</p>
-                    <p>Assistant Community Manager</p>
-                      <p>Responsible for customer service requests of property owners via phone and email</p>
-                </div>
+                <p>August 2019 - November 2020</p>
+                <p>Assistant Community Manager</p>
+                <p>Responsible for customer service requests of property owners via phone and email</p>
+              </div>
             </div>
-        </div>
-        <div className="fifthrow">
-        <div className={`job5 ${activeStates[4] ? 'active' : ''}`} onClick={() => handleJobClick(4)}>
+          </div>
+          <div className="fifthrow">
+            <div className={`job5 ${activeStates[4] ? 'active' : ''}`} onClick={() => handleJobClick(4)}>
               <div className="job5text">
                 <p><strong>Chili's</strong></p>
-                  <p>June 2018 - November 2020</p>
-                    <p>Server</p>
-                      <p>Greeting and serving guests</p>
-                </div>
+                <p>June 2018 - November 2020</p>
+                <p>Server</p>
+                <p>Greeting and serving guests</p>
+              </div>
             </div>
-        </div>
-        <div className="sixthrow">
-        <div className={`job6 ${activeStates[5] ? 'active' : ''}`} onClick={() => handleJobClick(5)}>
+          </div>
+          <div className="sixthrow">
+            <div className={`job6 ${activeStates[5] ? 'active' : ''}`} onClick={() => handleJobClick(5)}>
               <div className="job6text">
                 <p><strong>Next Model Management</strong></p>
-                  <p>May 2014 - August 2019</p>
-                    <p>Model</p>
-                      <p>Attending castings and auditions with potential clients, executing photoshoots/commercials</p>
-                </div>
+                <p>May 2014 - August 2019</p>
+                <p>Model</p>
+                <p>Attending castings and auditions with potential clients, executing photoshoots/commercials</p>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-</div>
+      </div>
 
 
 <Link to="https://docs.google.com/document/d/1CclG29IE5DrF9kvJ-8WkTxbu2opoSbUT-gZzfi63uOg/edit?usp=sharing">View resume</Link>
