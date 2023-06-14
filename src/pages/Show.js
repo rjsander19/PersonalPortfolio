1a07d591c346ee6c95ed9ca2948750d0b004b64a
rjsander <rjsander19@gmail.com>
2023-06-14 11:35:39 -0700

    adds edit form to show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNaviagte, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import '../Show.css';
 
